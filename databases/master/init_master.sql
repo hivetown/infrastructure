a599fedca52f7423cdbbf95707da29def79dcf3c
Romulo <romulo3218@gmail.com>
2023-04-10 18:12:56 +0100

    feat: adição da configuração do servidor de backuo

--- databases/master/init_master.sql
+++ databases/master/init_master.sql
@@ -1,3 +1,8 @@
 CREATE USER 'replicator_hivetown'@'%' IDENTIFIED WITH mysql_native_password BY 'hello2'; -- Cria um user para funcionar como replica
 GRANT REPLICATION SLAVE ON *.* TO 'replicator_hivetown'@'%'; -- Concede privilégios de replicação ao user
 FLUSH PRIVILEGES; -- Recarrrega as tabelas de privilégios
+
+CREATE USER 'mybackupuser'@'%' IDENTIFIED WITH mysql_native_password BY 'mybackuppassword'; -- Cria um user para funcionar como backup
+GRANT ALL PRIVILEGES ON hivetown.* TO 'mybackupuser'@'%';
+GRANT PROCESS ON *.* TO 'mybackupuser'@'%';
+FLUSH PRIVILEGES; -- Recarrega as tabelas de privilégios
