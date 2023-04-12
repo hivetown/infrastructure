4084ad312f30779de814bf30b8d5ca37461e1cf5
Lucas Pinto <github@lucks.pt>
2023-04-12 23:24:13 +0100

    fix: takeover script

--- load-balancers/backup/takeover.sh
+++ load-balancers/backup/takeover.sh
@@ -1,8 +1,8 @@
 # Unassign peer's IP aliases. Try it until it's possible.
-gcloud compute instances delete-access-config loadbalancer-master --zone=europe-west4-a > /etc/keepalived/takeover.log 2>&1
+gcloud compute instances delete-access-config loadbalancer-master --zone=europe-west4-a --access-config-name="External NAT" > /etc/keepalived/takeover.log 2>&1
 sleep 1
 # Assign IP aliases to me because now I am the MASTER!
-until gcloud compute instances add-access-config loadbalancer-backup --zone=europe-west4-b --address=34.90.28.85 >> /etc/keepalived/takeover.log 2>&1; do
+until gcloud compute instances add-access-config loadbalancer-backup --zone=europe-west4-b --address=34.90.28.85 --access-config-name="External NAT" >> /etc/keepalived/takeover.log 2>&1; do
  echo "Instance not accessible during takeover. Retrying in 5 seconds..."
  sleep 1
 done
