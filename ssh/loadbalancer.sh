5f9737c24b8f9a317abf369a96752fcec4689178
Lucas Pinto <github@lucks.pt>
2023-04-19 12:30:20 +0100

    Revert "fix: better scripts?"
    This reverts commit a9b644afeeda60e5aa336a82ef5b9adbf800b422.

--- ssh/loadbalancer.sh
+++
@@ -1,1 +0,0 @@
-gcloud compute ssh --zone "europe-west4-$2" "ubuntu@loadbalancer-$1" --tunnel-through-iap --project "hivetown"
--- ssh/servicediscovery.sh
+++
@@ -1,1 +0,0 @@
-gcloud compute ssh --zone "europe-west4-a" "ubuntu@servicediscovery-$1" --tunnel-through-iap --project "hivetown"
