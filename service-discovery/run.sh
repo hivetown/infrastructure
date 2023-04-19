41b1b9285ab41d46f2ff127d044eaabf342d5898
Lucas Pinto <github@lucks.pt>
2023-04-19 01:12:13 +0100

    chore: use a different port

--- service-discovery/run.sh
+++ service-discovery/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --name hivetown-zookeeper -p 2181:2181 -p 8080:8080 hivetown-zookeeper
+docker run --rm -it --name hivetown-zookeeper -p 2181:2181 -p 8888:8080 hivetown-zookeeper
