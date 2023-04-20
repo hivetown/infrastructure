8cf3f37c0a75b81d49cb5745cd07be2ff387114b
Lucas Pinto <github@lucks.pt>
2023-04-20 21:44:16 +0100

    feat: databases ssh

---
+++ ssh/vm-database-1.sh
@@ -0,0 +1,1 @@
+gcloud compute ssh --zone "europe-west4-a" "romul@vm-database-1" --tunnel-through-iap --project "hivetown"
---
+++ ssh/vm-database-backup.sh
@@ -0,0 +1,1 @@
+gcloud compute ssh --zone "us-east1-b" "romul@vm-database-backup" --tunnel-through-iap --project "hivetown"
