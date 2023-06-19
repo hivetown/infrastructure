4d174bf70c477d4d94e1392ffd92ccf50da9dd1c
Lucas Pinto <github@lucks.pt>
2023-06-19 10:59:19 +0100

    Update entrypoint.sh

--- web-servers/entrypoint.sh
+++ web-servers/entrypoint.sh
@@ -1,7 +1,13 @@
 # Copy .env files
-cp .env.example .env
+if [ ! -f .env.example ]
+then
+	cp .env.example .env
+fi
 # api.env, for now, already has the correct values predefined
-cp api.env.example api.env
+if [ ! -f .env.example ]
+then
+	cp api.env.example api.env
+fi
 
 # Get ens4 IP
 IP=$(ip addr show ens4 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
